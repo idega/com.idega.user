d392592b37cef780478f5812b845d59b6b0d124c
Gudmundur Agust Saemundsson <gummi@idega.com>
2002-06-07 11:12:28 +0000

    no message

---
+++ src/java/com/idega/user/presentation/BacicUserOverviewPresentationState.java
@@ -0,0 +1,75 @@
+package com.idega.user.presentation;
+
+import java.util.Iterator;
+import java.util.List;
+import com.idega.presentation.Page;
+import javax.swing.event.ChangeListener;
+import com.idega.idegaweb.IWException;
+import com.idega.event.*;
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="gummi@idega.is">Guðmundur Ágúst Sæmundsson</a>
+ * @version 1.0
+ */
+
+public class BacicUserOverviewPresentationState extends IWPresentationStateImpl implements IWActionListener {
+
+
+  String color1 = "00FF00";
+  String color2 = "FF0000";
+  String color = color1;
+
+  public BacicUserOverviewPresentationState() {
+
+  }
+
+
+  public String getColor(){
+    return color;
+  }
+
+
+
+  public boolean actionPerformed(IWPresentationEvent e)throws IWException{
+//    System.out.println("!!!!ÍJHHAAAAAAAAA!!!!!!!!!!!!!!");
+
+    if(color == color1){
+      color = color2;
+    } else {
+      color = color1;
+    }
+
+//    Page pg = e.getPage();
+//     boolean remove = false;
+//    if(e.getPage() instanceof ChangeListener ){
+//      System.out.println("is ChangeListener: true");
+//      this.addChangeListener((ChangeListener)e.getPage());
+//      remove = true;
+//    }
+
+//    else {
+//      System.out.println("is ChangeListener: false");
+//      List l = pg.getAllContainingObjects();
+//      if(l != null){
+//        Iterator iter = l.iterator();
+//        while (iter.hasNext()) {
+//          Object item = iter.next();
+//          if(item instanceof ChangeListener ){
+//            this.addChangeListener((ChangeListener)item);
+//          }
+//        }
+//      }
+//    }
+    this.fireStateChanged();
+//    if(remove){
+//      this.removeChangeListener((ChangeListener)e.getPage());
+//    }
+    return true;
+  }
+
+
+}
