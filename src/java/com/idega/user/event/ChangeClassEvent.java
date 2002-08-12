16b10dba48460b6650a9bbe06301b6b94b1215db
Thorhallur Helgason <laddi@idega.com>
2002-08-12 21:40:32 +0000

    An event for the main window to simply instanciate a new class. Similar to the behavior of

---
+++ src/java/com/idega/user/event/ChangeClassEvent.java
@@ -0,0 +1,48 @@
+package com.idega.user.event;
+
+import com.idega.presentation.IWContext;
+import com.idega.idegaweb.IWMainApplication;
+import com.idega.event.*;
+
+
+/**
+ * <p>Title: com.idega.user.event.ChangeClassEvent</p>
+ * <p>Description: Event that contains a class to instanciate similar to ObjectInstanciator</p>
+ * <p>Copyright: (c) 2002</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="eiki@idega.is">EirikurS. Hrafnsson</a>
+ * @version 1.0
+ */
+
+public class ChangeClassEvent extends IWPresentationEvent {
+	
+  private final String ENCRYPTED_CLASS_PARAMETER = IWMainApplication.classToInstanciateParameter;
+  private String _className;
+
+  public void setChangeClass(Class theClass){
+    addParameter(ENCRYPTED_CLASS_PARAMETER,IWMainApplication.getEncryptedClassName(theClass));
+  }
+  
+  public String getChangeClassName(){
+    return _className;
+  }
+  
+  private void setChangeClassName(String theClass){
+    _className = theClass;
+  }
+
+
+  public boolean initializeEvent(IWContext iwc) {
+    //get the encrypted class name
+    String className = iwc.getParameter(IWMainApplication.classToInstanciateParameter);
+   	if( className != null ){
+   		setChangeClassName(IWMainApplication.decryptClassName(className));
+ 		return true;
+   	}
+    
+    return false;
+
+
+  }
+
+}
