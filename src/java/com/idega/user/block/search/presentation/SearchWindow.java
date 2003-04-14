be99e4dbda3af23fb740cdb8edb174fcd99eb6fb
Eirikur Hrafnsson <eiki@idega.com>
2003-04-14 23:45:48 +0000

    *** empty log message ***

--- src/java/com/idega/user/block/search/presentation/SearchWindow.java
+++ src/java/com/idega/user/block/search/presentation/SearchWindow.java
@@ -137,7 +137,7 @@ public class SearchWindow extends IWAdminWindow implements ToolbarElement {
 		
 		SelectionBox groupSel = new SelectionBox(UserSearchEvent.SEARCH_FIELD_GROUPS);
 		groupSel.setHeight(13);
-		//TODO Eiki temporary make a method that only returns all groups with view permission and owner (edit)
+
 		Collection groupsCol = getUserBusiness(iwc).getAllGroupsWithViewPermission(iwc.getCurrentUser(),iwc);
 		
 		Iterator nodes = groupsCol.iterator();
