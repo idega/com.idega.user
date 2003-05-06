c2dcac5b69b0fcaf54945d4f8e9c8dce8eac3226
Eirikur Hrafnsson <eiki@idega.com>
2003-05-06 04:39:44 +0000

    first version of a simple window to create new postal codes. does not support deleting older codes yet.

---
+++ src/java/com/idega/user/presentation/PostalCodeEditorWindow.java
@@ -0,0 +1,186 @@
+package com.idega.user.presentation;
+
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
+
+import com.idega.business.IBOLookup;
+import com.idega.core.business.AddressBusiness;
+import com.idega.event.IWActionListener;
+import com.idega.event.IWStateMachine;
+import com.idega.idegaweb.IWBundle;
+import com.idega.idegaweb.IWConstants;
+import com.idega.idegaweb.IWMainApplication;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.idegaweb.presentation.IWAdminWindow;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Image;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Text;
+import com.idega.presentation.ui.CloseButton;
+import com.idega.presentation.ui.CountryDropdownMenu;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.HiddenInput;
+import com.idega.presentation.ui.SelectionBox;
+import com.idega.presentation.ui.SubmitButton;
+import com.idega.presentation.ui.TextInput;
+import com.idega.user.app.ToolbarElement;
+import com.idega.user.app.UserApplication;
+import com.idega.user.app.UserApplicationMainArea;
+import com.idega.user.app.UserApplicationMainAreaPS;
+import com.idega.user.block.search.event.UserSearchEvent;
+import com.idega.user.business.GroupBusiness;
+import com.idega.user.business.UserBusiness;
+import com.idega.user.data.Group;
+
+
+/**
+ * <p>Title: Postal code editor window</p>
+ * <p>Description: A simple editor window to add postal codes for any local</p>
+ * <p>Copyright: Idega Software Copyright (c) 2003</p>
+ * <p>Company: Idega Software</p>
+ * @author <a href="eiki@idega.is">Eirikur Hrafnsson</a>
+ * @version 1.0 
+ */
+public class PostalCodeEditorWindow extends IWAdminWindow{
+
+	private static final String PARAM_POSTAL_CODE = "postal_edwin_code";
+	private static final String PARAM_COUNTRY_ID= "postal_edwin_country_id";
+	private static final String PARAM_AREA = "postal_edwin_area";
+	
+	private static final String PARAM_SAVE = "postal_edwin_save";
+	
+	private AddressBusiness addressBiz;
+	private IWResourceBundle iwrb = null;
+	
+
+	private static final String IW_BUNDLE_IDENTIFIER = "com.idega.user";
+  
+
+	public PostalCodeEditorWindow() {
+		setWidth(300);
+		setHeight(160);
+		setScrollbar(false);
+		setResizable(true);
+	}
+
+	
+	public void main(IWContext iwc) throws Exception {
+		iwrb = getResourceBundle(iwc);
+	
+		if(iwc.isParameterSet(PARAM_SAVE)){
+			String postalCode = iwc.getParameter(PARAM_POSTAL_CODE);
+			String area = iwc.getParameter(PARAM_AREA);
+			String countryID = iwc.getParameter(PARAM_COUNTRY_ID);
+			
+			
+			addressBiz = getAddressBusiness(iwc);
+			try {
+				addressBiz.getPostalCodeAndCreateIfDoesNotExist(postalCode,area,addressBiz.getCountryHome().findByPrimaryKey(new Integer(countryID)));
+				close();
+			}
+			catch (NumberFormatException e) {
+				e.printStackTrace();
+				add(iwrb.getLocalizedString("postalcodeeditorwindow.error_message","The postal code was not saved because of an error, please notify your system administrator"));
+			}
+			catch (RemoteException e) {
+				e.printStackTrace();
+				add(iwrb.getLocalizedString("postalcodeeditorwindow.error_message","The postal code was not saved because of an error, please notify your system administrator"));
+			}
+			catch (CreateException e) {
+				e.printStackTrace();
+				add(iwrb.getLocalizedString("postalcodeeditorwindow.error_message","The postal code was not saved because of an error, please notify your system administrator"));
+			}
+			catch (FinderException e) {
+				e.printStackTrace();
+				add(iwrb.getLocalizedString("postalcodeeditorwindow.error_message","The postal code was not saved because of an error, please notify your system administrator"));
+			}
+			
+
+		}
+		else{
+			Form form = new Form();
+	
+			setTitle(iwrb.getLocalizedString("postalcodeeditorwindow.title", "Postal codes"));
+			addTitle(iwrb.getLocalizedString("postalcodeeditorwindow.title", "Postal codes"), IWConstants.BUILDER_FONT_STYLE_TITLE);
+			setName(iwrb.getLocalizedString("postalcodeeditorwindow.title", "Postal codes"));
+	
+			add(form);
+			Table tab = new Table(2,4);
+			form.add(tab);
+			
+			tab.setColumnVerticalAlignment(1, Table.VERTICAL_ALIGN_TOP);
+			tab.setColumnVerticalAlignment(2, Table.VERTICAL_ALIGN_TOP);
+	
+			tab.setCellspacing(3);
+			//tab.setAlignment(2, 4, Table.HORIZONTAL_ALIGN_RIGHT);
+			tab.setWidth(Table.HUNDRED_PERCENT);
+			tab.setHeight(Table.HUNDRED_PERCENT);
+			
+			
+			Text codeText = new Text();
+			codeText.setText(iwrb.getLocalizedString("postalcodeeditorwindow.postal_code", "Code"));
+			codeText.setFontStyle(IWConstants.BUILDER_FONT_STYLE_LARGE);
+					
+			TextInput codeInput = new TextInput(PARAM_POSTAL_CODE);
+			codeInput.setStyleAttribute(IWConstants.BUILDER_FONT_STYLE_INTERFACE);
+			codeInput.setAsNotEmpty(iwrb.getLocalizedString("postalcodeeditorwindow.confirm_message", "You must fill in all the fields first."));
+			
+			tab.add(codeText, 1, 1);
+			tab.add(codeInput, 1, 2);
+			
+			Text countryText = new Text();
+			countryText.setText(iwrb.getLocalizedString("postalcodeeditorwindow.country", "Country"));
+			countryText.setFontStyle(IWConstants.BUILDER_FONT_STYLE_LARGE);
+				
+			CountryDropdownMenu countryMenu = new CountryDropdownMenu(PARAM_COUNTRY_ID);
+			countryMenu.setStyleAttribute(IWConstants.BUILDER_FONT_STYLE_INTERFACE);
+	
+			tab.add(countryText, 2, 1);
+			tab.add(countryMenu, 2, 2);
+			
+			Text areaText = new Text();
+			areaText.setText(iwrb.getLocalizedString("postalcodeeditorwindow.area", "Area"));
+			areaText.setFontStyle(IWConstants.BUILDER_FONT_STYLE_LARGE);
+				
+			TextInput areaInput = new TextInput(PARAM_AREA);
+			areaInput.setStyleAttribute(IWConstants.BUILDER_FONT_STYLE_INTERFACE);
+			areaInput.setAsNotEmpty(iwrb.getLocalizedString("postalcodeeditorwindow.confirm_message", "You must fill in all the fields first."));
+	
+			tab.add(areaText, 1, 3);
+			tab.add(areaInput, 1, 4);
+			
+			//buttons
+			SubmitButton save = new SubmitButton(iwrb.getLocalizedImageButton("postalcodeeditorwindow.save","save"), PARAM_SAVE,"true");
+	   	CloseButton close = new CloseButton(iwrb.getLocalizedImageButton("postalcodeeditorwindow.close", "close") );
+	    
+			tab.add(close, 2, 4);
+			tab.add(Text.getNonBrakingSpace(), 2, 4);
+			tab.add(save, 2, 4);
+		}
+				
+	}
+
+
+	public String getBundleIdentifier() {
+		return IW_BUNDLE_IDENTIFIER;
+	}
+
+	public AddressBusiness getAddressBusiness(IWContext iwc) {
+		if(addressBiz==null){	
+			try {
+				addressBiz = (AddressBusiness) IBOLookup.getServiceInstance(iwc,AddressBusiness.class);
+			}
+			catch (RemoteException e) {
+				e.printStackTrace();
+			}	
+		}	
+		return addressBiz;
+	}
+  
+}
