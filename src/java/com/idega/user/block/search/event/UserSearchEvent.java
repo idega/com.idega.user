367e3b6fb5f9baee0e1f5de5df4fe95ecc7e691e
Eirikur Hrafnsson <eiki@idega.com>
2003-05-06 06:49:04 +0000

    *** empty log message ***

--- src/java/com/idega/user/block/search/event/UserSearchEvent.java
+++ src/java/com/idega/user/block/search/event/UserSearchEvent.java
@@ -5,12 +5,12 @@ import com.idega.event.*;
 
 
 /**
- * <p>Title: idegaWeb</p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Title: UserSearchEvent</p>
+ * <p>Description: A search event for the user system</p>
+ * <p>Copyright: Idega Software Copyright (c) 2003</p>
  * <p>Company: idega Software</p>
- * @author <a href="gummi@idega.is">Guðmundur Ágúst Sæmundsson</a>
- * @version 1.0
+ * @author <a href="gummi@idega.is">Eirikur Hrafnsson</a>
+ * @version 1.1
  */
 
 public class UserSearchEvent extends IWPresentationEvent
@@ -20,6 +20,9 @@ public class UserSearchEvent extends IWPresentationEvent
 	public static final String SEARCH_FIELD_SEARCH_TYPE = "usr_search_type";
 	
 	public static final String SEARCH_FIELD_SIMPLE_SEARCH_STRING = "usr_search";
+	public static final String SEARCH_FIELD_FIRST_NAME = "usr_search_f_name";
+	public static final String SEARCH_FIELD_MIDDLE_NAME = "usr_search_m_name";
+	public static final String SEARCH_FIELD_LAST_NAME = "usr_search_l_name";
 	public static final String SEARCH_FIELD_GROUPS = "usr_search_groups";
 	public static final String SEARCH_FIELD_STATUS_ID = "usr_search_status_id";
 	public static final String SEARCH_FIELD_GENDER_ID = "usr_search_gender_id";
@@ -42,6 +45,9 @@ public class UserSearchEvent extends IWPresentationEvent
 	private int ageCeil = 0;
 	private String personalId = null;
 	private String address = null;
+	private String firstName = null;
+	private String middleName = null;
+	private String lastName = null;
 
   public UserSearchEvent(){
   }
@@ -72,33 +78,24 @@ public class UserSearchEvent extends IWPresentationEvent
 					
 				}
 				else if( searchType == this.SEARCHTYPE_ADVANCED ){// advanced search
-					searchString = iwc.getParameter(SEARCH_FIELD_SIMPLE_SEARCH_STRING);
+					
+					firstName = iwc.getParameter(SEARCH_FIELD_FIRST_NAME);
+					middleName = iwc.getParameter(SEARCH_FIELD_FIRST_NAME);
+					lastName = iwc.getParameter(SEARCH_FIELD_LAST_NAME);
 					groups = iwc.getParameterValues(this.SEARCH_FIELD_GROUPS);
+					genderId = Integer.parseInt(iwc.getParameter(SEARCH_FIELD_GENDER_ID));
+					personalId = iwc.getParameter(SEARCH_FIELD_PERSONAL_ID);
+					address = iwc.getParameter(SEARCH_FIELD_ADDRESS);
 					
-					if( iwc.isParameterSet(SEARCH_FIELD_STATUS_ID)){
-						statusId = Integer.parseInt(iwc.getParameter(SEARCH_FIELD_STATUS_ID));
-					}
-					
-					if( iwc.isParameterSet(SEARCH_FIELD_GENDER_ID)){
-						genderId = Integer.parseInt(iwc.getParameter(SEARCH_FIELD_GENDER_ID));
-					}
-
 					if( iwc.isParameterSet(SEARCH_FIELD_AGE_FLOOR)){
 						ageFloor = Integer.parseInt(iwc.getParameter(SEARCH_FIELD_AGE_FLOOR));
 					}
-					
 					if( iwc.isParameterSet(SEARCH_FIELD_AGE_CEILING)){
 						ageCeil = Integer.parseInt(iwc.getParameter(SEARCH_FIELD_AGE_CEILING));
+					}	
+					if( iwc.isParameterSet(SEARCH_FIELD_STATUS_ID)){
+						statusId = Integer.parseInt(iwc.getParameter(SEARCH_FIELD_STATUS_ID));
 					}
-					
-					if( iwc.isParameterSet(SEARCH_FIELD_PERSONAL_ID)){
-						personalId = iwc.getParameter(SEARCH_FIELD_PERSONAL_ID);
-					}
-					
-					if( iwc.isParameterSet(SEARCH_FIELD_ADDRESS)){
-						address = iwc.getParameter(SEARCH_FIELD_ADDRESS);
-					}		
-					
 					
 				}
 				else return false;//NO TYPE
@@ -225,6 +222,48 @@ public class UserSearchEvent extends IWPresentationEvent
 	 */
 	public void setPersonalId(String string) {
 		personalId = string;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setFirstName(String string) {
+		firstName = string;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setLastName(String string) {
+		lastName = string;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setMiddleName(String string) {
+		middleName = string;
 	}
 
 }
