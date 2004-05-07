3f7eb0206c13dc247f65120594fc8ec4257771cc
Gudmundur Agust Saemundsson <gummi@idega.com>
2004-05-07 14:55:44 +0000

    Reformed support for accesscontrol on DPT generated pages

--- src/java/com/idega/user/block/homepage/business/HomePageBusiness.java
+++ src/java/com/idega/user/block/homepage/business/HomePageBusiness.java
@@ -3,8 +3,6 @@ package com.idega.user.block.homepage.business;
 
 public interface HomePageBusiness extends com.idega.business.IBOService
 {
- public void createHomePage(com.idega.presentation.IWContext p0,com.idega.user.data.Group p1,com.idega.builder.dynamicpagetrigger.data.PageTriggerInfo p2)throws java.rmi.RemoteException,Exception, java.rmi.RemoteException;
- public int getCurrentGroupID(com.idega.presentation.IWContext p0)throws java.lang.Exception, java.rmi.RemoteException;
- public java.util.List getGroupDPTPageLinks(int[] p0)throws java.sql.SQLException, java.rmi.RemoteException;
+ public void createHomePage(com.idega.presentation.IWContext p0,com.idega.user.data.Group p1,com.idega.builder.dynamicpagetrigger.data.PageTriggerInfo p2)throws java.rmi.RemoteException,java.lang.Exception, java.rmi.RemoteException;
  public boolean invalidateGroup(com.idega.presentation.IWContext p0,com.idega.user.data.Group p1)throws com.idega.data.IDOLookupException, java.rmi.RemoteException;
 }
