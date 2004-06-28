9c9067d1bd4f2dbc1e00761a71a20230f37a3509
Thomas Hilbig <thomas@idega.com>
2004-06-28 14:07:04 +0000

    changed import for ICPropertyHandler

--- src/java/com/idega/user/handler/UserHandler.java
+++ src/java/com/idega/user/handler/UserHandler.java
@@ -2,7 +2,7 @@ package com.idega.user.handler;
 
 import java.util.List;
 
-import com.idega.core.builder.data.ICPropertyHandler;
+import com.idega.core.builder.presentation.ICPropertyHandler;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.PresentationObject;
 import com.idega.user.presentation.UserChooser;
