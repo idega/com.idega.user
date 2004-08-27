03d7dfd947e6bef81878da72201835cfa0f8cf7e
Thomas Hilbig <thomas@idega.com>
2004-08-27 15:31:41 +0000

    implementation of plugin for Toolbar

---
+++ src/java/com/idega/user/presentation/MassMovingWindowPlugin.java
@@ -0,0 +1,69 @@
+package com.idega.user.presentation;
+
+import java.util.Map;
+import com.idega.idegaweb.IWBundle;
+import com.idega.idegaweb.IWMainApplicationSettings;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Image;
+import com.idega.user.app.ToolbarElement;
+
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
+ * Created on Aug 27, 2004
+ */
+public class MassMovingWindowPlugin implements ToolbarElement {
+
+	private static final String IW_BUNDLE_IDENTIFIER = "com.idega.user";
+	
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getButtonImage(com.idega.presentation.IWContext)
+	 */
+	public Image getButtonImage(IWContext iwc) {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getName(com.idega.presentation.IWContext)
+	 */
+	public String getName(IWContext iwc) {
+		IWBundle bundle = iwc.getApplicationContext().getIWMainApplication().getBundle(IW_BUNDLE_IDENTIFIER);
+		IWResourceBundle resourceBundle = bundle.getResourceBundle(iwc);
+		return resourceBundle.getLocalizedString("button.massMoving", "Move");
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getPresentationObjectClass(com.idega.presentation.IWContext)
+	 */
+	public Class getPresentationObjectClass(IWContext iwc) {
+		return MassMovingWindow.class;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getParameterMap(com.idega.presentation.IWContext)
+	 */
+	public Map getParameterMap(IWContext iwc) {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#isValid(com.idega.presentation.IWContext)
+	 */
+	public boolean isValid(IWContext iwc) {
+		IWMainApplicationSettings settings = iwc.getApplicationSettings();
+		return (settings.getProperty("temp_show_is_related_stuff") != null);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.user.app.ToolbarElement#getPriority(com.idega.presentation.IWContext)
+	 */
+	public int getPriority(IWContext iwc) {
+		return 6;
+	}
+}
