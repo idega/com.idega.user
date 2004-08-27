8141a2dfecb0cca00b518516f6b942a7aa169325
Birna Jonsdottir <birna@idega.com>
2004-08-27 09:17:05 +0000

    icon changed from magnify.gif to magnifyingglass.gif

--- src/java/com/idega/user/presentation/UserChooserBrowser.java
+++ src/java/com/idega/user/presentation/UserChooserBrowser.java
@@ -17,7 +17,7 @@ import com.idega.presentation.ui.AbstractChooser;
 
 public class UserChooserBrowser extends AbstractChooser {
 	
-	private String imgName = "magnify.gif";
+	private String imgName = "magnifyingglass.gif";
 	private boolean isUserBundle = false;
 	
   public UserChooserBrowser(String chooserName) {
