40080f8efd2b44cb555305b7d55f231026cd7915
Thomas Hilbig <thomas@idega.com>
2004-09-02 16:54:20 +0000

    moved HomepageGenerator from user bundle to developer bundle

--- src/java/com/idega/user/block/homepage/business/HomePageBusiness.java
+++
@@ -1,8 +0,0 @@
-package com.idega.user.block.homepage.business;
-
-
-public interface HomePageBusiness extends com.idega.business.IBOService
-{
- public void createHomePage(com.idega.presentation.IWContext p0,com.idega.user.data.Group p1,com.idega.builder.dynamicpagetrigger.data.PageTriggerInfo p2)throws java.rmi.RemoteException,java.lang.Exception, java.rmi.RemoteException;
- public boolean invalidateGroup(com.idega.presentation.IWContext p0,com.idega.user.data.Group p1)throws com.idega.data.IDOLookupException, java.rmi.RemoteException;
-}
--- src/java/com/idega/user/block/homepage/business/HomePageBusinessHome.java
+++
@@ -1,8 +0,0 @@
-package com.idega.user.block.homepage.business;
-
-
-public interface HomePageBusinessHome extends com.idega.business.IBOHome
-{
- public HomePageBusiness create() throws javax.ejb.CreateException, java.rmi.RemoteException;
-
-}
