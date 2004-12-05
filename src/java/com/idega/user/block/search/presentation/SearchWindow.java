21888c027a3b8331204ad7fd9422487308e9ccfe
Thorhallur Helgason <laddi@idega.com>
2004-12-05 09:46:44 +0000

    Removed unused import.

--- src/java/com/idega/user/block/search/presentation/SearchWindow.java
+++ src/java/com/idega/user/block/search/presentation/SearchWindow.java
@@ -1,7 +1,6 @@
 package com.idega.user.block.search.presentation;
 
 import java.rmi.RemoteException;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
