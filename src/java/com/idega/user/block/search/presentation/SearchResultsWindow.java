ffdd97ecbb59d921ea77bc575ec4c5123ffce764
Thorhallur Helgason <laddi@idega.com>
2007-05-29 08:11:10 +0000

    *** empty log message ***

--- src/java/com/idega/user/block/search/presentation/SearchResultsWindow.java
+++ src/java/com/idega/user/block/search/presentation/SearchResultsWindow.java
@@ -1,10 +1,9 @@
 /**
- * Title:        A window class that displays the results of a search
- * Copyright:    Idega Software Copyright (c) 2003
- * Company:      Idega Software
+ * Title: A window class that displays the results of a search Copyright: Idega Software Copyright (c) 2003 Company: Idega Software
+ * 
  * @author <a href="mailto:eiki@idega.is">Eirikur Hrafnsson</a>
  * @version 1.0
-
+ * 
  * 
  */
 package com.idega.user.block.search.presentation;
@@ -21,80 +20,84 @@ import com.idega.user.block.search.business.SearchEngine;
 import com.idega.user.presentation.BasicUserOverview;
 import com.idega.user.presentation.StyledBasicUserOverViewToolbar;
 
-
 public class SearchResultsWindow extends BasicUserOverview {
-  
-  private static final String IW_BUNDLE_IDENTIFIER = "com.idega.user";
 
-  public SearchResultsWindow() {
-  }
-  
-	 
-	/* (non-Javadoc)
+	private static final String IW_BUNDLE_IDENTIFIER = "com.idega.user";
+
+	public SearchResultsWindow() {
+	}
+
+	public String getBundleIdentifier() {
+		return IW_BUNDLE_IDENTIFIER;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.idega.user.presentation.BasicUserOverview#getEntries(com.idega.presentation.IWContext)
 	 */
 	protected Collection getEntries(IWContext iwc) {
-		SearchResultsWindowPS sPs = (SearchResultsWindowPS)this.ps;
-		 try {
-		 	SearchEngine engine = getSearchEngine(iwc);
-			return engine.getResult( sPs.getLastUserSearchEvent() );
+		SearchResultsWindowPS sPs = (SearchResultsWindowPS) this.ps;
+		try {
+			SearchEngine engine = getSearchEngine(iwc);
+			return engine.getResult(sPs.getLastUserSearchEvent());
 		}
 		catch (RemoteException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	protected String getEntityBrowserIdentifier(){
+
+	protected String getEntityBrowserIdentifier() {
 		String identifier = "search-";
-		SearchResultsWindowPS sPs = (SearchResultsWindowPS)this.ps;
-		if(sPs!=null){
-			identifier+= sPs.getSearchString();
-		}	
-		
+		SearchResultsWindowPS sPs = (SearchResultsWindowPS) this.ps;
+		if (sPs != null) {
+			identifier += sPs.getSearchString();
+		}
+
 		return identifier;
 	}
-	
+
 	protected PresentationObject getEmptyListPresentationObject() {
 		Text text = new Text(this.iwrb.getLocalizedString("searchresultswindow.search_had_no_match", "The search did not return any results"));
-		
+
 		return text;
 	}
-	
 
 	public static SearchEngine getSearchEngine(IWApplicationContext iwc) {
 		SearchEngine business = null;
-			try {
-				business = (SearchEngine) IBOLookup.getServiceInstance(iwc, SearchEngine.class);
-			}
-			catch (RemoteException rme) {
-				rme.printStackTrace();
-			}
-			
+		try {
+			business = (SearchEngine) IBOLookup.getServiceInstance(iwc, SearchEngine.class);
+		}
+		catch (RemoteException rme) {
+			rme.printStackTrace();
+		}
+
 		return business;
 	}
-	
+
 	public Class getPresentationStateClass() {
 		return SearchResultsWindowPS.class;
 	}
-	
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.idega.user.presentation.BasicUserOverview#getToolbar()
 	 */
 	protected StyledBasicUserOverViewToolbar getToolbar() {
 		StyledBasicUserOverViewToolbar toolbar = super.getToolbar();
-		SearchResultsWindowPS sPs = (SearchResultsWindowPS)this.ps;
-		if(sPs!=null && this.iwrb!=null){
+		SearchResultsWindowPS sPs = (SearchResultsWindowPS) this.ps;
+		if (sPs != null && this.iwrb != null) {
 			String search = sPs.getSearchString();
-			if(search!=null ) {
-				toolbar.setTitle(this.iwrb.getLocalizedString("searchresultswindow.search:","Search : ")+search+ Text.getNonBrakingSpace(2));
+			if (search != null) {
+				toolbar.setTitle(this.iwrb.getLocalizedString("searchresultswindow.search:", "Search : ") + search + Text.getNonBrakingSpace(2));
 			}
-			else{
-				toolbar.setTitle(this.iwrb.getLocalizedString("searchresultswindow.advanced_search:","Advanced search")+Text.getNonBrakingSpace(2));	
+			else {
+				toolbar.setTitle(this.iwrb.getLocalizedString("searchresultswindow.advanced_search:", "Advanced search") + Text.getNonBrakingSpace(2));
 			}
 		}
-		
+
 		return toolbar;
 	}
 
--- src/java/com/idega/user/presentation/AdministratorPropertyWindow.java
+++ src/java/com/idega/user/presentation/AdministratorPropertyWindow.java
@@ -1,10 +1,9 @@
 /*
- * $Id: AdministratorPropertyWindow.java,v 1.8.2.1 2007/04/23 17:53:52 eiki Exp $
- *
+ * $Id: AdministratorPropertyWindow.java,v 1.8.2.2 2007/05/29 08:11:10 laddi Exp $
+ * 
  * Copyright (C) 2001-2005 Idega Software hf. All Rights Reserved.
- *
- * This software is the proprietary information of Idega hf.
- * Use is subject to license terms.
+ * 
+ * This software is the proprietary information of Idega hf. Use is subject to license terms.
  */
 package com.idega.user.presentation;
 
@@ -13,24 +12,23 @@ import com.idega.presentation.TabbedPropertyPanel;
 
 /**
  * <p>
- * This is a special implementation of the Usser property window that is only opened
- * for the Super Administrator user.
+ * This is a special implementation of the Usser property window that is only opened for the Super Administrator user.
  * </p>
- * Last modified: $Date: 2007/04/23 17:53:52 $ by $Author: eiki $
+ * Last modified: $Date: 2007/05/29 08:11:10 $ by $Author: laddi $
  * 
  * @author <a href="mailto:gummi@idega.is">Gudmundur Agust Saemundsson</a>,<a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
- * @version $Revision: 1.8.2.1 $
+ * @version $Revision: 1.8.2.2 $
  */
 public class AdministratorPropertyWindow extends UserPropertyWindow {
 
-	public final String SESSION_ADDRESS = "ic_admin_property_window";
-	
+	public final String IW_SESSION_ADDRESS1 = "ic_admin_property_window";
+
 	public AdministratorPropertyWindow() {
 		super();
 	}
 
 	public String getSessionAddressString() {
-		return this.SESSION_ADDRESS;
+		return this.IW_SESSION_ADDRESS1;
 	}
 
 	/**
@@ -39,14 +37,14 @@ public class AdministratorPropertyWindow extends UserPropertyWindow {
 	 * </p>
 	 */
 	public void initializePanel(IWContext iwc, TabbedPropertyPanel panel) {
-		
+
 		initializeUserAndGroup(iwc);
-		
+
 		UserLoginTab ult = new UserLoginTab();
 		ult.setPanel(panel);
 		ult.setUserID(getUserId());
 		ult.setGroupID(getGroupId());
-		
+
 		panel.addTab(ult, 0, iwc);
 	}
 }
