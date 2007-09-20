ee6789b5663a6fe2273a4f3488e0b048091b0b15
Eirikur Hrafnsson <eiki@idega.com>
2007-09-20 13:44:51 +0000

    merging with stuff from 3.1 that was missing

--- src/java/com/idega/user/IWBundleStarter.java
+++ src/java/com/idega/user/IWBundleStarter.java
@@ -1,11 +1,13 @@
 package com.idega.user;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.logging.Logger;
 
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
+import com.idega.business.IBORuntimeException;
 import com.idega.core.accesscontrol.business.StandardRoles;
 import com.idega.core.view.DefaultViewNode;
 import com.idega.core.view.FramedWindowClassViewNode;
@@ -16,8 +18,10 @@ import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
 import com.idega.idegaweb.IWMainApplication;
+import com.idega.idegaweb.IWUserContext;
 import com.idega.repository.data.RefactorClassRegistry;
 import com.idega.user.business.UserAppDWREventListener;
+import com.idega.user.business.UserBusiness;
 
 /**
  * A starter for adding the user app workspace node and more
@@ -72,26 +76,53 @@ public class IWBundleStarter implements IWBundleStartable {
 		
 	}
 	
-	protected void registerFramedUserApp(IWMainApplication iwma){
+	protected void registerFramedUserApp(IWMainApplication iwma){			
 		try {
 			ViewManager viewManager = ViewManager.getInstance(iwma);
+
 			Class applicationClass = RefactorClassRegistry.forName("com.idega.user.app.UserApplication");
-			FramedWindowClassViewNode userNode = new FramedWindowClassViewNode("user",viewManager.getWorkspaceRoot());
-			userNode.setKeyboardShortcut(new KeyboardShortcut("1"));
 			
-			Collection<String> roles = new ArrayList<String>();
+			//inline class to override the default hasUserAccess to check to top node view access
+			FramedWindowClassViewNode userNode = new FramedWindowClassViewNode("user",viewManager.getWorkspaceRoot()){
+			
+				public boolean hasUserAccess(IWUserContext iwuc){
+					try {
+						return getUserBusiness().hasTopNodes(iwuc.getCurrentUser(), iwuc);
+					} catch (RemoteException e) {
+						e.printStackTrace();
+					}
+					return false;
+				}
+				
+				public UserBusiness getUserBusiness() {
+					try {
+						return (UserBusiness) IBOLookup.getServiceInstance(IWMainApplication.getDefaultIWApplicationContext(), UserBusiness.class);
+					}
+					catch (IBOLookupException ile) {
+						throw new IBORuntimeException(ile);
+					}
+				}
+
+			};
+			
+			
+			userNode.setKeyboardShortcut(new KeyboardShortcut("1"));
+			userNode.setName("#{localizedStrings['com.idega.user']['iwapplication_name.UserApplication']}");
+			Collection roles = new ArrayList();
 			roles.add(StandardRoles.ROLE_KEY_USERADMIN);
 			userNode.setAuthorizedRoles(roles);
 			userNode.setWindowClass(applicationClass);
 			
 			String jspPath = iwma.getBundle("com.idega.workspace").getJSPURI("workspace.jsp");
 			userNode.setJspUri(jspPath);
+			
+			
 		}
 		catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-
+		
 	public void stop(IWBundle starterBundle) {
 	}
 
