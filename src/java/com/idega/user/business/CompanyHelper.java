3bd33f5d996738aff3d3cfd4549d3f1a289121d9
Valdas Žemaitis <valdas@idega.com>
2018-07-11 12:23:49 +0300

    New method to get company's address

--- src/java/com/idega/user/business/CompanyHelper.java
+++ src/java/com/idega/user/business/CompanyHelper.java
@@ -1,12 +1,15 @@
 package com.idega.user.business;
 
+import com.idega.core.location.data.Address;
 import com.idega.repository.data.Singleton;
 import com.idega.user.bean.UserDataBean;
 
 public interface CompanyHelper extends Singleton {
-	
+
 	public static final String SPRING_BEAN_IDENTIFIER = "companyHelperInUserBundle";
-	
+
 	public UserDataBean getCompanyInfo(String companyPersonalId);
+
+	public Address getCompanyAddress(String companyPersonalId);
 
 }
